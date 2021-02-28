0ee59bf8f990b6e5f6006d854390096b290cee55
Marcus Silva <mvfsillva@gmail.com>
2021-02-28 13:22:35 -0300

    feat: add main component

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,18 +1,120 @@
-// Packages
-import React from 'react'
-import { useTranslation } from 'react-i18next'
+import { useState } from 'react'
+import { CSVReader } from 'react-papaparse'
+import { toast } from 'react-toastify'
+import { withTheme } from 'styled-components'
 
-// Styles
+import Button from 'components/button'
+import Input from 'components/input'
+import { Container } from 'layout'
+import { currency, sumTime, onlyNumbers } from 'helpers'
+
 import * as S from './styles'
 
-const Main = () => {
-  const { t } = useTranslation()
+const Main = ({ theme }) => {
+  const [result, setResult] = useState([])
+  const [amount, setAmount] = useState<number | string>()
+  const [totalTime, setTotalTime] = useState<string | number>()
+  const [hourValue, setHourValue] = useState<string>()
+
+  const handleOnDrop = (data) => {
+    const [{ data: headers }] = data
+    const result = []
+
+    for (let i = 0; i < data.length; i++) {
+      const { data: currentLine } = data[i]
+      const obj = {}
+
+      for (let j = 0; j < headers.length; j++) {
+        if (currentLine[j]) {
+          obj[headers[j].toLowerCase().replace(/\s/g, '_')] = currentLine[j]
+        }
+      }
+
+      if (Object.keys(obj).length) {
+        result.push(obj)
+      }
+    }
+
+    const [, ...parsedResult] = result
+    setResult(parsedResult)
+  }
+
+  const handleOnError = () => {
+    return toast.error('Problem when trying to upload the file', {
+      style: {
+        fontSize: theme.font.sizes.xxsmall
+      }
+    })
+  }
+
+  const handleOnRemoveFile = () => {
+    return toast.warn('File removed', {
+      style: {
+        fontSize: theme.font.sizes.xxsmall
+      }
+    })
+  }
+
+  const normalizeData = (
+    data: Array<{ inicial: string; final: string; horas: string }>
+  ) => {
+    const filteredData = data.map(({ inicial, final, horas }) => {
+      return { start: inicial, end: final, total: horas }
+    })
+
+    return filteredData
+  }
+
+  const handleCheckHours = () => {
+    const hh = normalizeData(result)
+    const time = sumTime(hh)
+    const amountMinutes = (time.minutes / 60) * (+onlyNumbers(hourValue) / 100)
+    const amountHour = time.hours * (+onlyNumbers(hourValue) / 100)
+    const amount = (amountHour + amountMinutes) * 100
+    const totalWorkTime =
+      time.minutes > 0 ? `${time.hours}:${time.minutes}` : time.hours
+
+    setTotalTime(totalWorkTime)
+    setAmount(currency({ amount, currency: 'BRL' }))
+  }
 
   return (
-    <S.Wrapper>
-      <h1>{t('welcome')}</h1>
-    </S.Wrapper>
+    <Container>
+      <S.Wrapper>
+        <S.Field>
+          <Input
+            name="hourAmount"
+            placeholder="fill your hour value: R$ 250,00"
+            onChange={({ target: { value } }) => {
+              if (value) {
+                const val = +onlyNumbers(value)
+                value = currency({ amount: val, currency: 'BRL' })
+                setHourValue(value)
+              }
+            }}
+          />
+          <S.Dropzone>
+            <CSVReader
+              onDrop={handleOnDrop}
+              onError={handleOnError}
+              addRemoveButton
+              onRemoveFile={handleOnRemoveFile}
+            >
+              <span>Drop CSV file here to upload.</span>
+            </CSVReader>
+          </S.Dropzone>
+          <Button primary onClick={handleCheckHours}>
+            Check
+          </Button>
+        </S.Field>
+      </S.Wrapper>
+      <S.Section>
+        <span>Hour value: {hourValue}</span>
+        <span>Total hours worked: {totalTime}</span>
+        <span>Amount: {amount}</span>
+      </S.Section>
+    </Container>
   )
 }
 
-export default Main
+export default withTheme(Main)
--- src/components/main/styles.ts
+++ src/components/main/styles.ts
@@ -1,12 +1,67 @@
 import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+import { lighten } from 'polished'
 
 export const Wrapper = styled.main`
   ${({ theme }) => css`
-    display: flex;
-    align-items: center;
-    justify-content: center;
     padding: ${theme.spaces.medium};
-    font-size: ${theme.font.sizes.medium};
-    height: 60vh;
+    border: 1px solid ${lighten(0.7, theme.colors.primary)};
   `}
+`
+
+export const Label = styled.label`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.small};
+    margin-bottom: ${theme.spaces.small};
+  `}
+`
+
+export const Field = styled.div`
+  ${({ theme }) => css`
+    display: grid;
+    grid-template-columns: 1fr;
+    column-gap: ${theme.spaces.small};
+    row-gap: ${theme.spaces.small};
+    font-size: ${theme.spaces.xsmall};
+    align-items: center;
+
+    ${media.greaterThan('medium')`
+      grid-template-columns: 1fr 1fr 0.5fr;
+    `}
+  `}
+`
+
+export const Text = styled.span<{ bold?: boolean; margin?: string }>`
+  ${({ theme, bold, margin }) => css`
+    font-size: ${theme.font.sizes.small};
+    line-height: ${theme.font.sizes.small};
+    font-weight: ${bold && 500};
+    margin: ${margin};
+
+    &.text__medium {
+      font-size: ${theme.font.sizes.xmedium};
+    }
+  `}
+`
+
+export const Dropzone = styled.div`
+  display: flex;
+  align-items: center;
+  > div {
+    min-width: 100%;
+    max-height: 2rem;
+    border-radius: 8px !important;
+    width: auto;
+    div {
+      background: transparent !important;
+      width: 100% !important;
+    }
+  }
+`
+
+export const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  font-size: 2rem;
+  margin-top: 2rem;
 `
