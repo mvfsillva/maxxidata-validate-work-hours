59b0bf56fcbd0b667dcff6b685458b2633bfbf74
Marcus Silva <mvfsillva@gmail.com>
2021-02-28 13:23:00 -0300

    feat: update page with new components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,15 @@
-// Packages
-import React from 'react'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
-// Containers
 import Header from 'components/header'
+import Footer from 'components/footer'
 import Main from 'components/main'
 
 const Home = () => (
   <>
     <Header />
     <Main />
+    <Footer />
     <ToastContainer containerId="toastify" draggable={false} />
   </>
 )
